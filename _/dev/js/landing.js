54353501e473e931c9895f9a8b8d15a1005f0a59
Danya D. Smith <danyadsmith@gmail.com>
2016-07-17 19:34:12 -0400

    Implement forEach Utility Function and Refactor landing.js
    In this assignment, I created a new JavaScript file called utilities.js
    and created a forEach utility function that accepts an array and a
    function as a parameter. The forEach function then runs the callback
    function on each item in the array.
    
    Afterwards, I refactored the landing.js file to utilize this function.

--- _/dev/js/landing.js
+++ _/dev/js/landing.js
@@ -1,18 +1,27 @@
- 
-  var animatePoints = function(){
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var revealPoint = function(point){
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}; 
 
-    var revealPoint = function(x){
-      points[x].style.opacity = 1;
-      points[x].style.transform = "scaleX(1) translateY(x)";
-      points[x].style.msTransform = "scaleX(1) translateY(x)";
-      points[x].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };    
+var animatePoints = function(points){
+  forEach(points, revealPoint);
+};
 
-    for(var i = 0; i < points.length; i++){
-      revealPoint(i);
+window.onload = function(){
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation.
+  if(window.innerHeight > 950){
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event){
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
     }
-  };
-
-  setTimeout(animatePoints, 2000);
+  });
+};
