230adca724e8f68d6115d7e07d1abe031dbe4e88
Danya D. Smith <danyadsmith@gmail.com>
2016-07-26 19:57:01 -0400

    Refactor album.js to Resolve Errors
    The gulp build process revealed several errors in album.js. To
    correct them:
    
    * Change the opening and closing curly braces to brackets on
      lines 7, 13, 22 and 28
    * Add the missing concatenation symbols before the variable
      names on lines 34, 35 and 36
    * Change the album passed to the setCurrentAlbum function
      to confirm our code is working as expected

--- _/dev/js/album.js
+++ _/dev/js/album.js
@@ -4,13 +4,13 @@ var albumPicasso = {
   label: 'Cubism',
   year: '1881',
   albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: {
+  songs: [ 
     { title: 'Blue', duration: '4:26' },
     { title: 'Green', duration: '3:14' },
     { title: 'Red', duration: '5:01' },
     { title: 'Pink', duration: '3:21' },
     { title: 'Magenta', duration: '2:15' }
-  }
+  ]
 }
 
 var albumMarconi = {
@@ -19,21 +19,21 @@ var albumMarconi = {
   label: 'EM',
   year: '1909',
   albumArtUrl: 'assets/images/album_covers/20.png',
-  songs: {
+  songs: [
     { title: 'Hello Operator?', duration: '1:01' },
     { title: 'Ring, ring, ring', duration: '5:01' },
     { title: 'Fits in your pocket', duration: '3:21' },
     { title: 'Can you hear me now?', duration: '3:14' },
     { title: 'Wrong phone number', duration: '2:15' }
-  }
+  ]
 }
 
 var createSongRow = function(songNumber, songName, songLength){
   var template = 
       '<tr class="album-view-song-item">' 
-      + '  <td class="song-item-number">' songNumber + '</td>'
-      + '  <td class="song-item-title">' songName + '</td>'
-      + '  <td class="song-item-duration">' songLength + '</td>'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>';
 
   return template;
@@ -63,5 +63,5 @@ var createSongRow = function(songNumber, songName, songLength){
  };
  
  window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+     setCurrentAlbum(albumMarconi);
  };
