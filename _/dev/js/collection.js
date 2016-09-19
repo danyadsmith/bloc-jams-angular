b7f4592756e8054f52b177a64b8d13e2155a8c15
Danya D. Smith <danyadsmith@gmail.com>
2016-09-18 22:10:11 -0400

    Initial Refactor of Collection.js and Album.js with jQuery
    In this commit, the jQuery CDN 2.2.4 is added to collection.html and
    album.html. Next, we refactor collection.jw and album.js - removing some
    of the DOM scripting in favor of jQuery syntax.

--- _/dev/js/collection.js
+++ _/dev/js/collection.js
@@ -1,21 +1,25 @@
-var collectionItemTemplate = '  <div class="collection-album-container">' +
- '    <img src="assets/images/album_covers/01.png" alt="Album Cover">' +
- '    <div class="collection-album-info caption">' +
- '      <p>' +
- '        <a href="album.html" class="album-name">The Colors</a>' +
- '        <br>' +
- '        <a href="album.html">Pablo Picasso</a>' +
- '        <br />' +
- '        X songs' +
- '        <br />' +
- '      </p>' +
- '    </div>' +
- '  </div>';
-
-window.onload = function(){
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
-  collectionContainer.innerHTML = '';
-  for(var i = 0; i < 16; i++){
-    collectionContainer.innerHTML += collectionItemTemplate;
-  }
+var buildCollectionItemTemplate = function(){
+  var template = '  <div class="collection-album-container">' +
+    '<img src="assets/images/album_covers/01.png" alt="Album Cover">' +
+    '<div class="collection-album-info caption">' +
+    '<p>' +
+    '<a href="album.html" class="album-name">The Colors</a>' +
+    '<br>' +
+    '<a href="album.html">Pablo Picasso</a>' +
+    '<br />' +
+    'X songs' +
+    '<br />' +
+    '</p>' +
+    '</div>' +
+    '</div>';
+  return $(template);
 };
+
+$(window).load(function(){
+  var $collectionContainer = $('.album-covers');
+  $collectionContainer.empty();
+  for(var i = 0; i < 12; i++){
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
+  }
+});
