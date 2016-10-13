430a85581992ce8add40e2e70c07ddb0e2743678
Danya D. Smith <danyadsmith@gmail.com>
2016-10-12 20:44:25 -0400

    Restructure BlocJams Project Using AngularJS Framework
    Create an app directory to host the core deployment folder
    structure for an AngularJS single page application. In this
    revised version of BlocJams, the home page, collection page
    and album pages will be refactored into a SPA model using
    templates.
    
    Here we create the app directory structure:
    
    * assets
    * assets/images
    * scripts
    * styles
    * templates
    * index.html
    
    We add the Angular.js library via script tag to the HEAD
    section of the document, and test our build process.
    
    In this project, I have opted to use Gulp instead of Grunt.

---
+++ _/dev/js/app.js
@@ -0,0 +1,6 @@
+/* global angular  */
+
+angular
+  .module("blocJams", [
+    "ui.router"
+  ]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,82 @@
-var gulp = require('gulp');
-var beeper = require('beeper');
-var browsersync = require('browser-sync');
-var concat = require('gulp-concat');
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var jshint = require('gulp-jshint');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
+var gulp = require("gulp");
+var beeper = require("beeper");
+var browsersync = require("browser-sync");
+// var concat = require("gulp-concat");
+var del = require("del");
+var imagemin = require("gulp-imagemin");
+var jshint = require("gulp-jshint");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+// var source = require("vinyl-source-stream");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 
 function onError(err) {
   beeper();
   console.log(err);
 }
 
-gulp.task('browsersync', function(cb){
+gulp.task("browsersync", function(cb){
   return browsersync({
     server: {
-      baseDir: './'
+      baseDir: "./app"
     }
   }, cb);
 });
 
-gulp.task('clean', function(cb){
-  return del(['assets/*'], cb);
+gulp.task("clean", function(cb){
+  return del(["assets/*"], cb);
 });
 
-gulp.task('album_cover_images', function(){
-  return gulp.src('_/dev/img/album_covers/*')
+gulp.task("album_cover_images", function(){
+  return gulp.src("_/dev/img/album_covers/*")
     .pipe(imagemin())
-    .pipe(gulp.dest('assets/images/album_covers'));
+    .pipe(gulp.dest("app/assets/images/album_covers"));
 });
 
-gulp.task('background_images', function(){
-  return gulp.src('_/dev/img/blurred_backgrounds/*')
+gulp.task("background_images", function(){
+  return gulp.src("_/dev/img/blurred_backgrounds/*")
     .pipe(imagemin())
-    .pipe(gulp.dest('assets/images/blurred_backgrounds'));
+    .pipe(gulp.dest("app/assets/images/blurred_backgrounds"));
 });
 
-gulp.task('images', function(){
-  return gulp.src('_/dev/img/*')
+gulp.task("images", function(){
+  return gulp.src("_/dev/img/*")
     .pipe(imagemin())
-    .pipe(gulp.dest('assets/images')); 
+    .pipe(gulp.dest("app/assets/images")); 
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('_/dev/js/*.js')
+gulp.task("scripts", function() {
+  return gulp.src("_/dev/js/*.js")
     .pipe(sourcemaps.init())
-    //.pipe(concat('all.js'))
+    //.pipe(concat("all.js"))
     .pipe(jshint())
-    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter("default"))
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('assets/scripts'));
+    .pipe(gulp.dest("app/scripts"));
 });
 
-gulp.task('styles', function() {
-  return gulp.src('_/dev/scss/*.scss')
+gulp.task("styles", function() {
+  return gulp.src("_/dev/scss/*.scss")
     .pipe(plumber({
       errorHandler: onError
     }))
     .pipe(sass({
-        outputStyle: 'compressed',
-        includePaths: ['node_modules/susy/sass']
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('assets/styles'));
+      outputStyle: "compressed",
+      includePaths: ["node_modules/susy/sass"]
+    }).on("error", sass.logError))
+    .pipe(gulp.dest("app/styles"));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('*.html', browsersync.reload);
-  gulp.watch('_/dev/scss/*', 
-  gulp.series('styles', browsersync.reload));
-  gulp.watch('_/dev/js/*.js', 
-  gulp.series('scripts', browsersync.reload));
-  gulp.watch('_/dev/img/*', 
-  gulp.series('images', browsersync.reload));
+gulp.task("watch", function(){
+  gulp.watch("*.html", browsersync.reload);
+  gulp.watch("_/dev/scss/*", 
+  gulp.series("styles", browsersync.reload));
+  gulp.watch("_/dev/js/*.js", 
+  gulp.series("scripts", browsersync.reload));
+  gulp.watch("_/dev/img/*", 
+  gulp.series("images", browsersync.reload));
 });
 
-gulp.task('default', gulp.parallel('browsersync', 'styles', 'scripts', 'images', 'album_cover_images', 'background_images', 'watch'));
+gulp.task("default", gulp.parallel("browsersync", "styles", "scripts", "images", "album_cover_images", "background_images", "watch"));
